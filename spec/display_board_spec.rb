deb4fe43fbd4bfb8a1e93bbff23733d85ba33e87
Mark Cascardi <mcascardi@gmail.com>
2020-04-13 01:20:18 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -137,17 +137,23 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Xs' do
-      # Should you want to write your own test for this situation,
-      # read the following code and comments.
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
 
-      # Can you copy the syntax of the tests above to write a test for a board
-      # entirely filled with Xs?"
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
 
-      # Define the board with values that should create the desired output
-      # *** Edit the line below ***
-      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
+      expect(rows[0]).to eq(" X | X | X ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" X | X | X ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" X | X | X ")
+    end 
 
-      # Don't touch the following lines.
+      # 
+      # board = ["X", "X", "X", "v", "X", "X", "O", "X", "O"]
+      # board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
+      # 
+      # # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
 
@@ -175,15 +181,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(true).to be(true)
     end
 
-    it 'prints an entire board full of Os' do
-      # Can you copy the syntax of the tests above to write a test for a board
-      # entirely filled with Os?
-
-      # Hint: You should be able to copy the code in the previous it example
-      # and make a few simple edits to convert the previous example to this
-      # example's situation.
-
-      # *** Comment the line below by adding a # at the line start ***
+  
       expect(true).to be(true)
     end
   end
